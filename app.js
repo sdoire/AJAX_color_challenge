ca1a96ac406407ad6e40cc61f8c8a1a268a01f17
sdoire <sarah.doire@gmail.com>
2015-07-24 11:12:09 -0500

    changed text color

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         success: function(response){
             $.each(response, function(index, myObj) {
                 var $el = $('body').children().last();
-                $el.append("<div><p>" + myObj.color + "</p></div>");
+                $el.append("<div><p class='white'>" + myObj.color + "</p></div>");
                 var color = myObj.value;
                 $el.children().last().css("background-color",color);
             })
