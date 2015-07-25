77b2f55d7d4bd62f7680f33abec1a49f1c253244
sdoire <sarah.doire@gmail.com>
2015-07-25 13:22:29 -0500

    hard mode addition

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
+    var showList = ["red", "magenta", "yellow"];
+
     $.ajax('data.json', {
         success: function(response){
+            
+                
             $.each(response, function(index, myObj) {
                 var $el = $('body').children().last();
-                $el.append("<div><p class='white'>" + myObj.color + "</p></div>");
                 var color = myObj.value;
+                for (var i=0; i < showList.length; i++) {
+                if (myObj.color == showList[i]) {
+                $el.append("<div><p class='white'>" + myObj.color + "</p></div>");
                 $el.children().last().css("background-color",color);
-            })
+            };
+            };
+         });
 
         },
         error: function(request, errorType, errorMessage){
